2e4e1d359f92cc7d3be9f922e3cd620dad93e2b2
YWHo <YWHo@users.noreply.github.com>
2018-07-18 14:50:32 +1200

    initial. variables

---
+++ c001_hello_world.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace cSharp_language
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
---
+++ c005_escape_character.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EscapeChar {
+   class Program {
+      static void Main(string[] args) {
+         Console.WriteLine("Hello\tWorld\n\n");
+         Console.ReadLine();
+      }
+   }
+}
+
